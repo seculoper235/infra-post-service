dbed02d2839ef903ab42a29d8cdd1b295a66c116
seculoper <mir8725@naver.com>
2025-02-26 20:41:38 +0900

    project init.

---
+++ src/main/java/com/example/infrablogservice/InfraBlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.infrablogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraBlogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfraBlogServiceApplication.class, args);
+    }
+
+}
