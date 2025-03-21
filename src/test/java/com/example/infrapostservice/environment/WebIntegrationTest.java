c387dc6ea80646525cd078c52bc020cc5bf3137c
seculoper <mir8725@naver.com>
2025-03-21 23:27:12 +0900

    resolved #7 CORS 설정 구현

---
+++ src/test/java/com/example/infrapostservice/environment/WebIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.example.infrapostservice.environment;
+
+import org.junit.jupiter.api.Disabled;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Disabled
+public class WebIntegrationTest {
+}
